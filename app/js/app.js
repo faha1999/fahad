08534d3d0312d8348895ca1a29d9499edf51565e
faha1999 <kafahad1999@gmail.com>
2022-02-23 18:02:01 +0600

    complete work

--- app/js/app.js
+++ app/js/app.js
@@ -29,3 +29,23 @@ modalClose.forEach((mc) => {
     });
   });
 });
+
+// portfolio filter
+
+let mixerPortfolio = mixitup('.work_container', {
+  selectors: {
+    target: '.work_card'
+  },
+  animation: {
+    duration: 300
+  }
+});
+
+const linkWork = document.querySelectorAll('.work_item');
+
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove('active-work'));
+  this.classList.add('active-work');
+}
+
+linkWork.forEach((l) => l.addEventListener('click', activeWork));
