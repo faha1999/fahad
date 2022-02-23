a9f48766f0f263186b81814060835ac05aaebf7e
faha1999 <kafahad1999@gmail.com>
2022-02-23 15:53:27 +0600

    complete services

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,30 @@
 
 function scrollHeader() {
   const nav = document.getElementById('header');
-  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
   if (this.scrollY >= 80) nav.classList.add('scroll-header');
   else nav.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+// services modal
+const modalViews = document.querySelectorAll('.services_modal'),
+  modalBtn = document.querySelectorAll('.services_button'),
+  modalClose = document.querySelectorAll('.services_modal-close');
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtn.forEach((mb, i) => {
+  mb.addEventListener('click', () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener('click', () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove('active-modal');
+    });
+  });
+});
