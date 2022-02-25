050cdaa5677c00faa93087a4a843a96891e33565
faha1999 <kafahad1999@gmail.com>
2022-02-25 15:54:10 +0600

    added scroll active

--- app/js/app.js
+++ app/js/app.js
@@ -71,3 +71,27 @@ let swiperTestimonials = new Swiper('.testimonial_container', {
     }
   }
 });
+
+// Scroll active
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
